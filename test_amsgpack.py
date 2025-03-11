1db45e0e5f83f45d980eb8057d055710e2c207f5
Arseniy Terekhin <senyai@gmail.com>
2025-03-11 22:21:40 +0300

    refactor: format tests

--- test_amsgpack.py
+++ test_amsgpack.py
@@ -6,31 +6,31 @@ from msgpack import unpackb
 
 class PackbTest(TestCase):
     def test_true(self):
-        self.assertEqual(packb(True), b'\xc3')
+        self.assertEqual(packb(True), b"\xc3")
 
     def test_false(self):
-        self.assertEqual(packb(False), b'\xc2')
+        self.assertEqual(packb(False), b"\xc2")
 
     def test_none(self):
-        self.assertEqual(packb(None), b'\xc0')
+        self.assertEqual(packb(None), b"\xc0")
 
     def test_double(self):
-        self.assertEqual(packb(pi), b'\xcb@\t!\xfbTD-\x18')
+        self.assertEqual(packb(pi), b"\xcb@\t!\xfbTD-\x18")
 
     def test_i8(self):
         for i in range(-127, 128):
             self.assertEqual(unpackb(packb(i)), i)
 
     def test_i16(self):
-        for i in (-0x7fff, 0x1234, 0x8000):
+        for i in (-0x7FFF, 0x1234, 0x8000):
             self.assertEqual(unpackb(packb(i)), i)
 
     def test_i32(self):
-        for i in (-0x7fff_0000, 0x1000_0000):
+        for i in (-0x7FFF_0000, 0x1000_0000):
             self.assertEqual(unpackb(packb(i)), i)
 
     def test_i64(self):
-        for i in (-0x7fff_0000_0000_0000, 0x1000_0000_0000_0000):
+        for i in (-0x7FFF_0000_0000_0000, 0x1000_0000_0000_0000):
             self.assertEqual(unpackb(packb(i)), i)
 
     def test_ascii(self):
@@ -44,13 +44,13 @@ class PackbTest(TestCase):
     def test_str8(self):
         unicode = "a" * 0
         self.assertEqual(unpackb(packb(unicode)), unicode)
-        unicode = "a" * 0xff
+        unicode = "a" * 0xFF
         self.assertEqual(unpackb(packb(unicode)), unicode)
 
     def test_str16(self):
         unicode = "a" * 0x100
         self.assertEqual(unpackb(packb(unicode)), unicode)
-        unicode = "a" * 0xffff
+        unicode = "a" * 0xFFFF
         self.assertEqual(unpackb(packb(unicode)), unicode)
 
     def test_str32(self):
@@ -73,11 +73,11 @@ class PackbTest(TestCase):
 
     def test_bytes_0_to_20_el(self):
         for n in range(21):
-            value = 'A' * n
+            value = "A" * n
             self.assertEqual(unpackb(packb(value)), value)
 
     def test_bytes_other(self):
-        value = 'A' * 0xffff
+        value = "A" * 0xFFFF
         self.assertEqual(unpackb(packb(value)), value)
-        value = 'A' * 0x20000
+        value = "A" * 0x20000
         self.assertEqual(unpackb(packb(value)), value)
