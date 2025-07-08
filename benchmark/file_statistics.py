0870d8de5311804dd9c315d44191843dbc1a6efe
Arseniy Terekhin <senyai@gmail.com>
2025-07-08 22:28:06 +0300

    dev: print key statistics in `file_statistics.py`

--- benchmark/file_statistics.py
+++ benchmark/file_statistics.py
@@ -1,77 +1,95 @@
 #!/usr/bin/env python3
+from __future__ import annotations
 from typing import Any
 from collections import defaultdict
-from functools import reduce
+from operator import itemgetter
 
 
-def merge(
-    a: defaultdict[int, str], b: defaultdict[int, str]
-) -> defaultdict[int, str]:
-    out = a.copy()
-    for key, value in b.items():
-        out[key] += value
-    return out
+class Stat(defaultdict[str, int]):
+    def __init__(self, default_factory: type = int, *args: Any):
+        super().__init__(default_factory, *args)
+
+    def __add__(self, other: Stat) -> Stat:
+        out = self.copy()
+        for key, value in other.items():
+            out[key] += value
+        return out
+
+    def repr(self):
+        out: list[str] = []
+        for key, value in sorted(
+            self.items(), key=itemgetter(1), reverse=True
+        ):
+            out.append(f"{key} -> {value}")
+        return "\n".join(out)
 
 
 class FileStatistics:
     def __init__(self, obj: Any) -> None:
-        self.stats: defaultdict[int, str] = defaultdict[str](int)
-        self._gather(obj)
+        self.stats = Stat()
+        self.key_stats = Stat()
+        self._gather(obj, self.stats)
 
-    def _gather(self, obj: Any) -> None:
+    def __iadd__(self, other: FileStatistics):
+        self.stats += other.stats
+        self.key_stats += other.key_stats
+        return self
+
+    def _gather(self, obj: Any, stats: Stat) -> None:
         match obj:
             case str():
                 length = len(obj)
                 if length <= 31:
-                    self.stats["fixstr"] += 1
+                    stats["fixstr"] += 1
                 elif length <= 0xFF:
-                    self.stats["str 8"] += 1
+                    stats["str 8"] += 1
                 elif length <= 0xFFFF:
-                    self.stats["str 16"] += 1
+                    stats["str 16"] += 1
                 elif length <= 0xFFFFFFFF:
-                    self.stats["str 32"] += 1
+                    stats["str 32"] += 1
                 else:
                     raise ValueError(length)
             case float():
-                self.stats["float"] += 1
+                stats["float"] += 1
             case int():
                 if -32 <= obj <= 0x7F:
-                    self.stats["fixint"] += 1
+                    stats["fixint"] += 1
                 elif abs(obj) <= 0xFF:
-                    self.stats["int 8"] += 1
+                    stats["int 8"] += 1
                 elif abs(obj) <= 0xFFFF:
-                    self.stats["int 16"] += 1
+                    stats["int 16"] += 1
                 elif abs(obj) <= 0xFFFFFFFF:
-                    self.stats["int 32"] += 1
+                    stats["int 32"] += 1
                 elif abs(obj) <= 0xFFFFFFFFFFFFFFFF:
-                    self.stats["int 64"] += 1
+                    stats["int 64"] += 1
                 else:
                     raise ValueError(obj)
             case list():
                 length = len(obj)
                 if length <= 15:
-                    self.stats["fixarray"] += 1
+                    stats["fixarray"] += 1
                 elif length <= 0xFFFF:
-                    self.stats["array 16"] += 1
+                    stats["array 16"] += 1
                 elif length <= 0xFFFFFFFF:
-                    self.stats["array 32"] += 1
+                    stats["array 32"] += 1
                 else:
                     raise ValueError(length)
                 for item in obj:
-                    self._gather(item)
+                    self._gather(item, stats)
             case dict():
                 length = len(obj)
                 if length <= 15:
-                    self.stats["fixmap"] += 1
+                    stats["fixmap"] += 1
                 elif length <= 0xFFFF:
-                    self.stats["map 16"] += 1
+                    stats["map 16"] += 1
                 elif length <= 0xFFFFFFFF:
-                    self.stats["map 32"] += 1
+                    stats["map 32"] += 1
                 else:
                     raise ValueError(length)
                 for key, value in obj.items():
-                    self._gather(key)
-                    self._gather(value)
+                    self._gather(key, stats)
+                    self._gather(key, self.key_stats)
+                    self._gather(value, stats)
             case None:
                 self.stats["undefined"] += 1
             case _:
@@ -85,7 +103,7 @@ def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("paths", nargs="+", type=Path)
     args = parser.parse_args()
-    stats = []
+    file_statistics: FileStatistics | None = None
     for path in args.paths:
         match path.suffix:
             case ".json":
@@ -99,11 +117,21 @@ def main():
             case _:
                 raise ValueError(_)
 
-        stats.append(FileStatistics(data).stats)
+        stats = FileStatistics(data)
+        print(f"\n{path}:")
+        print(stats.stats.repr())
+        if file_statistics is None:
+            file_statistics = stats
+        else:
+            file_statistics += stats
 
-    res = reduce(merge, stats, defaultdict(int))
-    for key, value in sorted(res.items(), key=lambda v: v[1], reverse=True):
-        print(f"{key} -> {value}")
+    if file_statistics:
+        print(f"\nall {len(args.paths)} files")
+        print(file_statistics.stats.repr())
+        print("\nkeys stats")
+        print(file_statistics.key_stats.repr())
+    else:
+        print("no file statistics")
 
 
 if __name__ == "__main__":
