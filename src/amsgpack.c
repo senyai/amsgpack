1f8628461e3826bceedc9dbe06f9a4094609650a
Arseniy Terekhin <senyai@gmail.com>
2025-07-23 14:12:03 +0300

    release: 0.3.0

--- src/amsgpack.c
+++ src/amsgpack.c
@@ -53,7 +53,7 @@ static PyObject* AnyUnpacker_iter(PyObject* self) {
 #include "unpacker.h"
 // include unpacker before file_unpacker
 #include "file_unpacker.h"
-#define VERSION "0.2.0"
+#define VERSION "0.3.0"
 
 /*
   amsgpack module
